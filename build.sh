d266f0dba8c5eae1d47776faffb21afb35cdf52b
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2019-04-27 08:39:14 -0700

    no jekyll

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cd `dirname $0`
 cat cheat-sheet.adoc | awk 'BEGIN {output = 1} /^[=]+ / {output = 1} /== Examples/ {output = 0} /^\[\[/ {output = 1} {if (output) print}' > cheat-sheet-no-examples.adoc
 
 mkdir -p docs pdf
+touch docs/.nojekyll
 
 asciidoctor cheat-sheet.adoc && mv cheat-sheet.html docs/index.html
 asciidoctor-pdf cheat-sheet*.adoc && mv *.pdf pdf/
