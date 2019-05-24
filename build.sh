1ed1c9c3ce96b2f210c395d0b524f00b84b82943
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2019-05-24 09:39:48 -0700

    Added a 102 Cheat Sheet
    This is still considered work in progress, really need to put more thoughts into the advanced section.

--- build.sh
+++ build.sh
@@ -2,12 +2,25 @@
 
 cd `dirname $0`
 
-cat cheat-sheet.adoc | awk 'BEGIN {output = 1} /^[=]+ / {output = 1} /== Examples/ {output = 0} /^\[\[/ {output = 1} {if (output) print}' > cheat-sheet-no-examples.adoc
-
-mkdir -p docs pdf
+mkdir -p docs pdf && rm -rf {docs,pdf}/*
 touch docs/.nojekyll
 
-asciidoctor cheat-sheet.adoc && mv cheat-sheet.html docs/index.html
-asciidoctor-pdf cheat-sheet*.adoc && mv *.pdf pdf/
+cp index.html docs/
+
+for adoc in 1*.adoc
+do
+  name=`basename ${adoc} .adoc`
+  cat ${adoc} | awk 'BEGIN {output = 1} /^[=]+ / {output = 1} /== Examples/ {output = 0} /^\[\[/ {output = 1} {if (output) print}' > ${name}-no-examples.adoc
+  asciidoctor ${adoc} && mv ${name}.html docs/
+  # remove links from pdf files
+  for _adoc in ${name}*.adoc
+  do
+    _name=`basename ${_adoc} .adoc`
+    cat ${_adoc} | grep -v '^link:' > ${_name}-pdf.adoc
+    asciidoctor-pdf ${_name}-pdf.adoc && mv ${_name}-pdf.pdf pdf/${_name}.pdf
+    rm ${_name}-pdf.adoc
+  done
+  rm ${name}-no-examples.adoc
+done
 
 ls pdf/ | xargs -n1 -I {} pdfcrop --margins 50 pdf/{} pdf/{}
